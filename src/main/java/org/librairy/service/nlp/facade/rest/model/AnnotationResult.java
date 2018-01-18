5c09e00d9af925fb9386d32a409ec16e2b47fc79
Carlos Badenes <cbadenes@gmail.com>
2018-01-18 14:10:03 +0100

    handle common Annotations

--- src/main/java/org/librairy/service/nlp/facade/rest/model/AnnotationResult.java
+++ src/main/java/org/librairy/service/nlp/facade/rest/model/AnnotationResult.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author Badenes Olmedo, Carlos <cbadenes@fi.upm.es>
@@ -13,8 +12,8 @@ public class AnnotationResult {
 
     private final List<Annotation> annotations;
 
-    public AnnotationResult(List<org.librairy.service.nlp.facade.model.Annotation> annotations) {
-        this.annotations = annotations.stream().map( a -> new Annotation(a)).collect(Collectors.toList());
+    public AnnotationResult(List<Annotation> annotations) {
+        this.annotations = annotations;
     }
 
     @JsonProperty(required = true)
